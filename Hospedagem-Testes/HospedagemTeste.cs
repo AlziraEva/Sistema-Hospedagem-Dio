bc178c530e9f93ff5f8d144684af7c81d3aad0e7
Alzira Eva <alziralves1996@gmail.com>
2024-02-13 10:36:31 -0300

    Test: Implementação de testes unitários

--- Hospedagem-Testes/HospedagemTeste.cs
+++ Hospedagem-Testes/HospedagemTeste.cs
@@ -27,4 +27,102 @@ public class UnitTest1
         //Assert
         Assert.Equal(hospedes.Count(), reserva.Hospedes.Count());
     }
+
+    [Fact]
+    public void NaoCadastrar2HospedesParaCapacidadeSuiteDe1RetornarException()
+    {
+        //Arange
+        List<Pessoa> hospedes = new List<Pessoa>();
+
+        Pessoa p1 = new Pessoa(nome: "Hóspede 1");
+        Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+
+        hospedes.Add(p1);
+        hospedes.Add(p2);
+
+        Suite suite = new Suite(tipoSuite: "Premium", capacidade: 1, valorDiaria: 30);
+
+        Reserva reserva = new Reserva(diasReservados: 10);
+
+        //Act
+        reserva.CadastrarSuite(suite);
+
+        //Assert
+        Assert.Throws<ArgumentException>(() =>
+        {
+            reserva.CadastrarHospedes(hospedes);
+        });
+    }
+
+    [Fact]
+    public void ObterQuantidaHospedesNaReservaHospedesCadastrados2DeveSerIgualAhHospedesDaReserva2()
+    {
+        //Arange
+        List<Pessoa> hospedes = new List<Pessoa>();
+
+        Pessoa p1 = new Pessoa(nome: "Hóspede 1");
+        Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+
+        hospedes.Add(p1);
+        hospedes.Add(p2);
+
+        Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+
+        Reserva reserva = new Reserva(diasReservados: 10);
+
+        //Act
+        reserva.CadastrarSuite(suite);
+        reserva.CadastrarHospedes(hospedes);
+
+        //Assert
+        Assert.Equal(2, reserva.ObterQuantidadeHospedes());
+    }
+
+    [Fact]
+    public void AplicarDescontoDe10PorcentoEmValorDiario30ReaisPara10DiasReservadosRetornar270()
+    {
+        //Arange
+        List<Pessoa> hospedes = new List<Pessoa>();
+
+        Pessoa p1 = new Pessoa(nome: "Hóspede 1");
+        Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+
+        hospedes.Add(p1);
+        hospedes.Add(p2);
+
+        Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+
+        Reserva reserva = new Reserva(diasReservados: 10);
+
+        //Act
+        reserva.CadastrarSuite(suite);
+        reserva.CadastrarHospedes(hospedes);
+
+        //Assert
+        Assert.Equal(270, reserva.CalcularValorDiaria());
+    }
+
+    [Fact]
+    public void ValorDiaria30ReaiPara5DiasRetornar150()
+    {
+        //Arange
+        List<Pessoa> hospedes = new List<Pessoa>();
+
+        Pessoa p1 = new Pessoa(nome: "Hóspede 1");
+        Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+
+        hospedes.Add(p1);
+        hospedes.Add(p2);
+
+        Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+
+        Reserva reserva = new Reserva(diasReservados: 5);
+
+        //Act
+        reserva.CadastrarSuite(suite);
+        reserva.CadastrarHospedes(hospedes);
+
+        //Assert
+        Assert.Equal(150, reserva.CalcularValorDiaria());
+    }
 }
